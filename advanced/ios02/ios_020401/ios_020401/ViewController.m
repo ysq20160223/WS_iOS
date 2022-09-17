c8e8008d99add12f73bcbca07966dc2ad32070bc
ysq20160223 <398183931@qq.com>
2022-09-17 23:16:40 +0800

    remove ios_020401 andIsAdd param

--- advanced/ios02/ios_020401/ios_020401/ViewController.m
+++ advanced/ios02/ios_020401/ios_020401/ViewController.m
@@ -36,20 +36,22 @@
     
     _segmented.frame = CGRectMake(0, kScreenH - 3 * _segmented.xHeight, kScreenW, _segmented.xHeight);
     
-    [self adjustImagePositionWithColumns:[_segmented titleForSegmentAtIndex:_segmented.selectedSegmentIndex].intValue add:YES];
+    [self adjustImagePositionWithColumns:[_segmented titleForSegmentAtIndex:_segmented.selectedSegmentIndex].intValue andIsAdd:YES];
+//    [self adjustImagePositionWithColumns:[_segmented titleForSegmentAtIndex:_segmented.selectedSegmentIndex].intValue];
 }
 
 
 - (IBAction)indexChange:(UISegmentedControl *)sender {
-    NSLog(@"%ld; %@", sender.selectedSegmentIndex, [sender titleForSegmentAtIndex:sender.selectedSegmentIndex]);
+//    NSLog(@"%ld; %@", sender.selectedSegmentIndex, [sender titleForSegmentAtIndex:sender.selectedSegmentIndex]);
     
     [UIView animateWithDuration:1 animations:^{
-        [self adjustImagePositionWithColumns:[sender titleForSegmentAtIndex:sender.selectedSegmentIndex].intValue add:NO];
+//        [self adjustImagePositionWithColumns:[sender titleForSegmentAtIndex:sender.selectedSegmentIndex].intValue andIsAdd:NO];
+        [self adjustImagePositionWithColumns:[_segmented titleForSegmentAtIndex:_segmented.selectedSegmentIndex].intValue];
     }];
 }
 
-
-- (void)adjustImagePositionWithColumns:(long)columns add:(BOOL)add{
+//
+- (void)adjustImagePositionWithColumns:(long)columns andIsAdd:(BOOL)isAdd {
     CGFloat margin = (kScreenW - columns * kImgW) / (columns + 1); // 每个表情的间距
 
 //    NSArray<UIView *> *subviews = self.view.subviews;
@@ -64,7 +66,7 @@
         CGFloat x = margin + col * (kImgW + margin);
         CGFloat y = margin + row * (kImgW + margin) + kStatusBarH;
         
-        if(add) {
+        if(isAdd) {
             [self addImg:[NSString stringWithFormat:@"%d.png", i % 9] x:x y:y index:i];
         } else {
             UIView *vChild = self.view.subviews[i + 1];
@@ -75,6 +77,30 @@
         }
     }
 //    NSLog(@"subviews: %@", self.view.subviews);
+}
+
+// 无需传递 add 参数
+- (void)adjustImagePositionWithColumns:(long)columns {
+    CGFloat margin = (kScreenW - columns * kImgW) / (columns + 1); // 每个表情的间距
+
+    for (int i = 0; i < 9; i++) {
+        int col = i % columns; // i这个位置对应的列数
+        int row = i / columns;
+        
+        CGFloat x = margin + col * (kImgW + margin);
+        CGFloat y = margin + row * (kImgW + margin) + kStatusBarH;
+        
+        if(self.view.subviews.count < 10) {
+            [self addImg:[NSString stringWithFormat:@"%d.png", i % 9] x:x y:y index:i];
+        } else {
+            UIView *vChild = self.view.subviews[i + 1];
+            
+            CGRect rect = vChild.frame;
+            rect.origin = CGPointMake(x, y);
+            vChild.frame = rect;
+        }
+    }
+//    NSLog(@"subviews: %@; count: %ld", self.view.subviews, self.view.subviews.count);
 }
 
 
